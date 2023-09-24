fb7db9b6e89dc9fcc634701eb9c860ae2b74cf7e
melnikovknst <145424645+melnikovknst@users.noreply.github.com>
2023-09-25 00:08:10 +0700

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+void isSqr(int num, int *count) {
+    if(sqrt(num) == ceil(sqrt(num)))
+        *count += 1;
+}
+
+
+int main(void) {
+    int n, count = 0, *array;  //n -lenght of array
+    scanf("%d", &n);
+    array = (int *)malloc(n * sizeof(int));
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &array[i]);
+        isSqr(array[i], &count);
+    }
+    
+    printf("%d\n", count);
+    return 0;
+}
